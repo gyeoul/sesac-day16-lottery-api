66b979961e175cc5c7e41e7025f186ead8b8dd14
Gyeoul <clsmay.1st@gmail.com>
2023-08-08 18:38:17 +0900

    Initial Commit

---
+++ sesac-day16-lottery-api/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesac-day16-lottery-api
+//
+//  Created by Gyeoul on 2023/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
